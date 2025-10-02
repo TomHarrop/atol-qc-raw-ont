dfed53c008e8abb681c6364ce7937a2e948cca9f
Tom Harrop <twharrop@gmail.com>
2025-10-02 11:08:00 +1000

    scaffold

---
+++ README.md
@@ -0,0 +1,33 @@
+# atol-qc-raw-ont
+
+Run short read QC on Nanopore (ONT) reads.
+
+1. Trim adaptors (`porechop`)
+2. Filter reads (`filtlong`)
+3. Output stats
+
+## Installation: Use the [BioContainer](https://quay.io/repository/biocontainers/atol-qc-raw-ont?tab=tags)
+
+*e.g.* with Apptainer/Singularity:
+
+```bash
+apptainer exec \
+  docker://quay.io/biocontainers/atol-qc-raw-ont:0.1.0 \
+  atol-qc-raw-ont  
+  
+```
+
+## Usage
+
+TODO
+
+```bash
+atol-qc-raw-ont \
+```
+
+### Full usage
+
+TODO
+
+```
+```
---
+++ atol_qc_raw_ont/__main__.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from importlib import resources
+from importlib.metadata import metadata, files
+from pathlib import Path
+from snakemake.logging import logger
+import argparse
+from snakemake.api import (
+    SnakemakeApi,
+    ResourceSettings,
+    ConfigSettings,
+    ExecutionSettings,
+    OutputSettings,
+)
+from snakemake.settings.enums import Quietness
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+
+    # options
+    parser.add_argument("-t", "--threads", type=int, default=16, dest="threads")
+    parser.add_argument("-n", help="Dry run", dest="dry_run", action="store_true")
+
+    # inputs
+    input_group = parser.add_argument_group("Input")
+
+    input_group.add_argument(
+        "--in", required=True, type=Path, help="Read 1 input", dest="r1"
+    )
+
+    # outputs
+    output_group = parser.add_argument_group("Output")
+
+    output_group.add_argument(
+        "--out", required=True, type=Path, help="Read 1 output", dest="r1_out"
+    )
+    output_group.add_argument(
+        "--stats", required=True, type=Path, help="Stats output (json)", dest="stats"
+    )
+    output_group.add_argument(
+        "--logs",
+        required=False,
+        type=Path,
+        help="Log output directory. Default: logs are discarded.",
+        dest="logs_directory",
+    )
+
+    return parser.parse_args()
+
+
+def main():
+    # print version info
+    pkg_metadata = metadata(__package__)
+
+    pkg_name = pkg_metadata.get("Name")
+    pkg_version = pkg_metadata.get("Version")
+
+    logger.warning(f"{pkg_name} version {pkg_version}")
+
+    # get the snakefile
+    snakefile = Path(resources.files(__package__), "workflow", "Snakefile")
+    if snakefile.is_file():
+        logger.warning(f"Using snakefile {snakefile}")
+    else:
+        raise FileNotFoundError("Could not find a Snakefile")
+
+    stats_template = Path(
+        resources.files(__package__), "workflow", "report", "stats.json"
+    )
+    if stats_template.is_file():
+        logger.warning(f"Using stats_template {stats_template}")
+    else:
+        raise FileNotFoundError("Could not find a stats_template")
+
+    # get arguments
+    args = parse_arguments()
+    logger.warning(f"Entrypoint args:\n    {args}")
+    args.stats_template = stats_template
+
+    # control output
+    output_settings = OutputSettings(quiet={Quietness.RULES, Quietness.HOST})
+
+    # set cores.
+    resource_settings = ResourceSettings(
+        cores=args.threads,
+        overwrite_resource_scopes={
+            "mem": "global",
+            "threads": "global",
+        },
+    )
+
+    # other settings
+    config_settings = ConfigSettings(config=args.__dict__)
+    execution_settings = ExecutionSettings(args.__dict__, lock=False)
+
+    with SnakemakeApi(output_settings) as snakemake_api:
+        workflow_api = snakemake_api.workflow(
+            snakefile=snakefile,
+            resource_settings=resource_settings,
+            config_settings=config_settings,
+        )
+        dag = workflow_api.dag()
+        dag.execute_workflow(
+            executor="dryrun" if args.dry_run else "local",
+            execution_settings=execution_settings,
+        )
